14f19bc0824751675ff5110cf33229bec355150b
catalyst <39456203+catalyst2001@users.noreply.github.com>
2023-06-04 02:14:23 +0500

    shader

---
+++ common/shader.h
@@ -0,0 +1,15 @@
+#pragma once
+// glCreateShader
+// glShaderSource
+// glCompileShader
+// glGetShaderiv
+// glGetShaderInfoLog
+// glCreateProgram
+// glBindAttribLocation
+// glAttachShader
+// glLinkProgram
+// glGetProgramiv
+// glGetProgramInfoLog
+
+//glReleaseShaderCompiler OPT
+//glIsShader OPT
